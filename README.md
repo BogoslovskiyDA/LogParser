096c3e83365e79dbe13e5cc3093c86219eb72b66
BogoslovskiyDA <anmorgana.w@gmail.com>
2021-10-21 22:33:11 +0300

    Read file

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # LogParser
 
 My log parser for MY.GAMES Сareer
+
+##Options
+
+-p PATH, --path PATH	Read file/folder by specified PATH
--- parser.py
+++ parser.py
@@ -1,4 +1,27 @@
-from sys import argv
+import glob
+import os
+import sys
+import argparse
+
+
+def read_file(file):
+    f = open(file, 'r')
+    print(f.readlines())
+    f.close()
+
+
+def read_arguments():
+    arg_par = argparse.ArgumentParser()
+    arg_par.add_argument('-p', '--path')
+    return arg_par
+
 
 if __name__ == '__main__':
-    pass
+    arg_par = read_arguments()
+    path = arg_par.parse_args(sys.argv[1:])
+    files = glob.glob(os.path.join(path.path, '*.txt'))
+    if (files != []):
+        for filename in files:
+            read_file(filename)
+    else:
+        read_file(path.path)
